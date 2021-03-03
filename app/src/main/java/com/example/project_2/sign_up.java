91edf934ee5c910b4362a7dd218a3a91132afb0b
Manar-Olimat <44707799+Manar-Olimat@users.noreply.github.com>
2021-03-03 18:00:34 +0200

    test

--- app/src/main/java/com/example/project_2/sign_up.java
+++ app/src/main/java/com/example/project_2/sign_up.java
@@ -7,13 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
 public class sign_up extends AppCompatActivity {
-Button sign_up;
+    Button sign_up;
+    TextInputEditText gg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         sign_up=findViewById(R.id.sign_up);
+       gg=findViewById(R.id.TextInputEditText);
         sign_up.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
