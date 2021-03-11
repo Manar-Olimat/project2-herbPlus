61c09fa8bbe3bb939209b1cab76deb09e7e69811
Manar-Olimat <44707799+Manar-Olimat@users.noreply.github.com>
2021-03-11 22:41:33 +0200

    test_Ro'a

--- app/src/main/java/com/example/project_2/user_account.java
+++ app/src/main/java/com/example/project_2/user_account.java
@@ -2,13 +2,24 @@ package com.example.project_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class user_account extends AppCompatActivity {
-
+    RelativeLayout Notification;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_account);
+        Notification=findViewById(R.id.Notification);
+        Notification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainIntent = new Intent(user_account.this,home.class);
+                user_account.this.startActivity(mainIntent);
+            }
+        });
     }
 }
