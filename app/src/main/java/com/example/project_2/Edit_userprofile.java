1979c361e5858bbc1761677ed04d3de5a89231ff
Manar-Olimat <44707799+Manar-Olimat@users.noreply.github.com>
2021-03-12 14:28:30 +0200

    test_Ro'a

--- app/src/main/java/com/example/project_2/Edit_userprofile.java
+++ app/src/main/java/com/example/project_2/Edit_userprofile.java
@@ -2,13 +2,17 @@ package com.example.project_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class Edit_userprofile extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_userprefile);
+
     }
 }
--- app/src/main/java/com/example/project_2/user_account.java
+++ app/src/main/java/com/example/project_2/user_account.java
@@ -7,8 +7,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.RelativeLayout;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class user_account extends AppCompatActivity {
     RelativeLayout Notification;
+    FloatingActionButton edit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +22,15 @@ public class user_account extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent mainIntent = new Intent(user_account.this,home.class);
+                user_account.this.startActivity(mainIntent);
+            }
+        });
+
+        edit=findViewById(R.id.edit);
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainIntent = new Intent(user_account.this,Edit_userprofile.class);
                 user_account.this.startActivity(mainIntent);
             }
         });
