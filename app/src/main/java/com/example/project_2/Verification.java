07cfe20ce86820d3f565875aefd077b8cca8e7c9
Manar-Olimat <44707799+Manar-Olimat@users.noreply.github.com>
2021-04-07 13:34:28 +0300

    shatha

--- app/src/main/java/com/example/project_2/Verification.java
+++ app/src/main/java/com/example/project_2/Verification.java
@@ -3,6 +3,7 @@ package com.example.project_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class Verification extends AppCompatActivity {
 
@@ -10,5 +11,16 @@ public class Verification extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verification);
+
+        try {
+            GMailSender sender = new GMailSender("shathaolaimat@gmail.com", "1234");
+            sender.sendMail("test",
+                    "This is test code",
+                    "shathaolaimat@gmail.com",
+                    "shathaolaimat@gmail.com");
+        } catch (Exception e) {
+            Log.e("SendMail", e.getMessage(), e);
+        }
+
     }
 }
